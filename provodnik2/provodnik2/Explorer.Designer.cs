c8c3ebcda30a1a6887a0577b7f012f5964a57a8f
nublikaska <nublikaska@gmail.com>
2017-05-25 18:22:09 +0500

    Add files via upload

---
+++ provodnik2/provodnik2/Explorer.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace provodnik2
+{
+    partial class Explorer
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Explorer));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            this.splitContainer1.Panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.splitContainer1.Panel1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.splitContainer1.Panel1MinSize = 383;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer3);
+            this.splitContainer1.Size = new System.Drawing.Size(1264, 682);
+            this.splitContainer1.SplitterDistance = 383;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(383, 682);
+            this.treeView1.TabIndex = 1;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "papka.jpg");
+            this.imageList1.Images.SetKeyName(1, "avi.png");
+            this.imageList1.Images.SetKeyName(2, "bin.png");
+            this.imageList1.Images.SetKeyName(3, "dll.png");
+            this.imageList1.Images.SetKeyName(4, "doc.png");
+            this.imageList1.Images.SetKeyName(5, "docx.png");
+            this.imageList1.Images.SetKeyName(6, "exe.png");
+            this.imageList1.Images.SetKeyName(7, "inf.png");
+            this.imageList1.Images.SetKeyName(8, "iso.png");
+            this.imageList1.Images.SetKeyName(9, "jpeg.png");
+            this.imageList1.Images.SetKeyName(10, "jpg.png");
+            this.imageList1.Images.SetKeyName(11, "mp3.png");
+            this.imageList1.Images.SetKeyName(12, "mp4.png");
+            this.imageList1.Images.SetKeyName(13, "pdf.png");
+            this.imageList1.Images.SetKeyName(14, "png.png");
+            this.imageList1.Images.SetKeyName(15, "pps.png");
+            this.imageList1.Images.SetKeyName(16, "rar.png");
+            this.imageList1.Images.SetKeyName(17, "txt.png");
+            this.imageList1.Images.SetKeyName(18, "unknown.png");
+            this.imageList1.Images.SetKeyName(19, "xlsx.png");
+            this.imageList1.Images.SetKeyName(20, "zip.png");
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.splitContainer2);
+            this.splitContainer3.Panel1MinSize = 383;
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.splitContainer3.Panel2.Controls.Add(this.label2);
+            this.splitContainer3.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer3.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer3.Size = new System.Drawing.Size(877, 682);
+            this.splitContainer3.SplitterDistance = 383;
+            this.splitContainer3.TabIndex = 3;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.splitContainer2.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.splitContainer2.Panel2.Controls.Add(this.listView1);
+            this.splitContainer2.Panel2MinSize = 653;
+            this.splitContainer2.Size = new System.Drawing.Size(383, 682);
+            this.splitContainer2.SplitterDistance = 25;
+            this.splitContainer2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(383, 653);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseKlick);
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Имя";
+            this.columnHeader1.Width = 130;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Размер";
+            this.columnHeader2.Width = 97;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Дата";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Время";
+            this.columnHeader4.Width = 53;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(48, 73);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(2, 229);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(484, 272);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(163, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            // 
+            // Explorer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 682);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Explorer";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            this.splitContainer3.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ provodnik2/provodnik2/Explorer.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+
+namespace provodnik2
+{
+    public partial class Explorer : Form
+    {
+        private int iFiles = 0;
+        FileStream log;
+        public Explorer()
+        {
+            InitializeComponent();
+
+            treeView1.BeforeSelect += treeView1_BeforeSelect;
+            treeView1.BeforeExpand += treeView1_BeforeExpand;
+            listView1.MouseDoubleClick += listView1_MouseDoubleClick;
+            // заполняем дерево дисками
+            FillDriveNodes();
+        }
+
+        //событие после нажатия 1 кликом
+        void listView1_MouseKlick(object o, MouseEventArgs e)
+        {
+            string name = ((System.Windows.Forms.ListView)o).FocusedItem.Text;
+            string format = name.Substring(name.LastIndexOf(".") + 1);
+            if ((format == "png") || (format == "jpeg") || (format == "jpg"))
+            {
+                pictureBox1.Image = new Bitmap(label1.Text + name);
+                pictureBox1.Visible = true;
+                richTextBox1.Clear();
+                richTextBox1.Visible = false;
+            }
+            else if (format == "txt")
+            {
+                WritRichTextBox1(label1.Text + name);
+                richTextBox1.Visible = true;
+                pictureBox1.Visible = true;
+                pictureBox1.Image = imageList1.Images["txt.png"];
+            }
+            else
+            {
+                richTextBox1.Visible = false;
+                richTextBox1.Clear();
+                if (imageList1.Images.ContainsKey(format + ".png"))
+                    pictureBox1.Image = imageList1.Images[format + ".png"];
+                else
+                    pictureBox1.Image = imageList1.Images["unknown.png"];
+            }
+
+            label2.Text = "Формат: " + format;
+        }
+
+        // событие после нажатия 2 кликом
+        void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Process.Start( label1.Text + ((System.Windows.Forms.ListView)sender).FocusedItem.Text );
+            log = new FileStream(@"log.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(log);
+            writer.WriteLine(DateTime.Now.ToShortDateString() + "/" + DateTime.Now.ToShortTimeString() + ":" + label1.Text + ((System.Windows.Forms.ListView)sender).FocusedItem.Text);
+            writer.Close();
+        }
+
+        // событие перед раскрытием узла
+        void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            e.Node.Nodes.Clear();
+            string[] dirs;
+            try
+            {
+                if (Directory.Exists(e.Node.FullPath))
+                {
+                    dirs = Directory.GetDirectories(e.Node.FullPath);
+                    if (dirs.Length != 0)
+                    {
+                        for (int i = 0; i < dirs.Length; i++)
+                        {
+                            TreeNode dirNode = new TreeNode(new DirectoryInfo(dirs[i]).Name);
+                            FillTreeNode(dirNode, dirs[i]);
+                            e.Node.Nodes.Add(dirNode);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { }
+        }
+        // событие перед выделением узла
+        void treeView1_BeforeSelect(object sender, TreeViewCancelEventArgs e)
+        {
+            e.Node.Nodes.Clear();
+            string[] dirs;
+            try
+            {
+                if (Directory.Exists(e.Node.FullPath))
+                {
+                    dirs = Directory.GetDirectories(e.Node.FullPath);
+                    if (dirs.Length != 0)
+                    {
+                        for (int i = 0; i < dirs.Length; i++)
+                        {
+                            TreeNode dirNode = new TreeNode(new DirectoryInfo(dirs[i]).Name);
+                            FillTreeNode(dirNode, dirs[i]);
+                            e.Node.Nodes.Add(dirNode);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { }
+            label1.Text = e.Node.FullPath.ToString();
+            log = new FileStream(@"log.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(log);
+            writer.WriteLine(DateTime.Now.ToShortDateString() + "/" + DateTime.Now.ToShortTimeString() + ":" + e.Node.FullPath.ToString());
+            writer.Close();
+            AddFiles(e.Node.FullPath.ToString());            
+        }
+
+        // получаем все диски на компьютере
+        private void FillDriveNodes()
+        {
+            try
+            {
+                foreach (DriveInfo drive in DriveInfo.GetDrives())
+                {
+                    TreeNode driveNode = new TreeNode { Text = drive.Name };
+                    FillTreeNode(driveNode, drive.Name);
+                    treeView1.Nodes.Add(driveNode);
+                }
+            }
+            catch (Exception ex) { }
+        }
+        // получаем дочерние узлы для определенного узла
+        private void FillTreeNode(TreeNode driveNode, string path)
+        {
+            try
+            {
+                string[] dirs = Directory.GetDirectories(path);
+                foreach (string dir in dirs)
+                {
+                    TreeNode dirNode = new TreeNode();
+                    dirNode.Text = dir.Remove(0, dir.LastIndexOf("\\") + 1);
+                    driveNode.Nodes.Add(dirNode);
+                }
+            }
+            catch (Exception ex) { }
+        }
+        //добавление файлов в listView
+        private void AddFiles(string strPath)
+        {
+            listView1.BeginUpdate();
+
+            listView1.Items.Clear();
+            iFiles = 0;
+            try
+            {
+                DirectoryInfo di = new DirectoryInfo(strPath + "\\");
+                FileInfo[] theFiles = di.GetFiles();
+                foreach (FileInfo theFile in theFiles)
+                {
+                    iFiles++;
+                    System.Windows.Forms.ListViewItem lvItem = new System.Windows.Forms.ListViewItem(theFile.Name);
+                    lvItem.SubItems.Add(theFile.Length.ToString());
+                    lvItem.SubItems.Add(theFile.LastWriteTime.ToShortDateString());
+                    lvItem.SubItems.Add(theFile.LastWriteTime.ToShortTimeString());                                    
+                    string loc = lvItem.Text;
+                    string format = loc.Substring(loc.LastIndexOf(".") + 1) + ".png";
+                    if (imageList1.Images.ContainsKey(format))
+                        lvItem.ImageKey = format;
+                    else
+                        lvItem.ImageKey = "unknown.png";
+                    listView1.Items.Add(lvItem);
+                }
+            }
+            catch (Exception Exc) {}
+
+            listView1.EndUpdate();
+        }
+        // чтение из файла
+        public void WritRichTextBox1(string filePath)
+        {
+            try
+            {
+                richTextBox1.Clear();
+                string[] lines = File.ReadAllLines(filePath, Encoding.GetEncoding(1251));
+                for (int i = 0; i < lines.Length; i++)
+                    richTextBox1.Text += lines[i] + "\n";
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
